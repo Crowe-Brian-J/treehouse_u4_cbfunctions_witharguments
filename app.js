20b256b37cc95ea821b1aadca170dbfd9d66cc1c
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-05 22:14:39 -0400

    use addStyleToElement to add red to div1, blue to div2 - create makeGreen function, make div3 green

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ function makeBlue(element) {
   element.style.backgroundColor = 'blue'
 }
 
+//create makeGreen callback function
+const makeGreen = (element) => {
+  element.style.backgroundColor = 'green'
+}
+
 function addStyleToElement(element, callback) {
   callback(element)
 }
+
+//when you need to add arguments to callback functions, add them separately to the called function's arguments (see above)
+//follow below
+
+addStyleToElement(div1, makeRed)
+addStyleToElement(div2, makeBlue)
+//add makeGreen to div3
+addStyleToElement(div3, makeGreen)
